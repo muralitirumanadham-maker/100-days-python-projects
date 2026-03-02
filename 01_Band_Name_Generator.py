b57954ffdb7ff0c614fe6f6a5741d26a2da696a1
murali krishna-AI <muralitirumanadham@gmail.com>
2026-03-02 06:20:57 +0530

    Delete 01_Band_Name_Generator.py

--- 01_Band_Name_Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator.")
-city=input("Whats the name of the city you grew up in?\n")
-pet_name=input("Whats your pet's name?\n")
-print(f"Your band name coud be {city} {pet_name}")
